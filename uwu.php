f4d30dab69e49c2765e5c792d845c4243edccf14
CrCrate <14haslo@gmail.com>
2020-04-28 21:07:51 +0200

    Update uwu.php

--- uwu.php
+++ uwu.php
@@ -1,13 +1,6 @@
-<html>
-<head>
-<title>BoxEntryway</title>
-</head>
-<body>
-<?php echo '<p>Laura Ping
+Laura Ping
 bman5757
 powerprimate
 flapper22172
 p4596070
-jimmymullen</p>'; ?>
-</body>
-</html>
+jimmymullen
